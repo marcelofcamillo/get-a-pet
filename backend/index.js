2edf4b462a39c7ba26ef13979582643a1bc8e435
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-04-02 22:35:40 -0300

    Configuração do projeto

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+// config JSON response
+app.use(express.json());
+
+// solve cors
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
+// public folder for images
+app.use(express.static('public'));
+
+app.listen(5000, () => console.log('API started!'));
