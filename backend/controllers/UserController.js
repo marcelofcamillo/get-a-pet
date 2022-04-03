a553bdf1aad2b222c2faacaada684001df7e3934
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-04-02 23:29:27 -0300

    UserController e UserRoutes

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,9 @@
+import User from '../models/User.js';
+
+class UserController {
+  static async register(req, res) {
+    res.json('Olá, Get a Pet!');
+  }
+}
+
+export default UserController;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import UserRoutes from './routes/UserRoutes.js';
 
 const app = express();
 
@@ -11,5 +12,8 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 // public folder for images
 app.use(express.static('public'));
+
+// routes
+app.use('/users', UserRoutes);
 
 app.listen(5000, () => console.log('API started!'));
