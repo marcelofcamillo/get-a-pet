760a70dd1ce6ae4c058fff0f2d430166d15f8fa8
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-04-03 23:03:10 -0300

    Removendo Pet do sistema

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -103,6 +103,7 @@ class PetController {
   static async getPetById(req, res) {
     const id = req.params.id;
 
+    // check if id is valid
     if (!ObjectId.isValidObjectId(id)) {
       res.status(422).json({ message: 'ID inválido!' });
       return;
@@ -117,6 +118,40 @@ class PetController {
     }
 
     res.status(200).json({ pet });
+  }
+
+  static async removePetById(req, res) {
+    const id = req.params.id;
+
+    // check if id is valid
+    if (!ObjectId.isValidObjectId(id)) {
+      res.status(422).json({ message: 'ID inválido!' });
+      return;
+    }
+
+    // check if pet exists
+    const pet = await Pet.findOne({ _id: id });
+
+    if (!pet) {
+      res.status(404).json({ message: 'Pet não encontrado!' });
+      return;
+    }
+
+    // check if logged in user registered the pet
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if (pet.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({
+        message:
+          'Houve um problema em processar a sua solicitação! Tente novamente mais tarde.',
+      });
+      return;
+    }
+
+    await Pet.findByIdAndRemove(id);
+
+    res.status(200).json({ message: 'Pet removido com sucesso!' });
   }
 }
 
