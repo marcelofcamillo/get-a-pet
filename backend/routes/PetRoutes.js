7c5911916d463973f636d3ea44cea97f4571ee9f
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-04-04 20:20:05 -0300

    Atualização do Pet

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -18,5 +18,11 @@ router.get('/mypets', verifyToken, PetController.getAllUserPets);
 router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions);
 router.get('/:id', PetController.getPetById);
 router.delete('/:id', verifyToken, PetController.removePetById);
+router.patch(
+  '/:id',
+  verifyToken,
+  imageUpload.array('images'),
+  PetController.updatePet
+);
 
 export default router;
