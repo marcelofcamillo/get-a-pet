2ce7f6d320f2442be3645444b828bd90dcbecbf9
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-04-09 21:57:32 -0300

    Criando componente de mensagem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { UserProvider } from './context/UserContext';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Container from './components/layout/Container';
+import Message from './components/layout/Message';
 
 // pages
 import Login from './components/pages/Auth/Login';
@@ -18,6 +19,7 @@ function App() {
     <Router>
       <UserProvider>
         <Navbar />
+        <Message />
         <Container>
           <Routes>
             <Route path="/login" element={<Login />} />
