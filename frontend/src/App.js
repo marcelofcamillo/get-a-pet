ba4880e5bbc57fb767bbfe7e4e777ccc2c76cf39
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-04-23 09:24:34 -0300

    Criando página de perfil

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import Message from './components/layout/Message';
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 import Home from './components/pages/Home';
+import Profile from './components/pages/User/Profile';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/user/profile" element={<Profile />} />
             <Route path="/" element={<Home />} />
           </Routes>
         </Container>
